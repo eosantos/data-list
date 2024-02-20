1040aaf7ebb3f17dde80317fee576a13d80ae406
eduardooliveira <eos.eduardooliveira@gmail.com>
2024-02-20 18:35:02 -0300

    Created component tabs

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { Plus } from 'lucide-react'
 import { Header } from './components/header'
+import { Tabs } from './components/tabs'
 
 export function App() {
 
@@ -7,6 +8,7 @@ export function App() {
     <div className="py-10 space-y-8">
       <div>
         <Header />
+        <Tabs />
       </div>
 
       <main className="max-w-6xl mx-auto space-y-5">
