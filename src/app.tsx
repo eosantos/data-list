4e0b331ff4c484ed9cd3ac0ee8479a5ff3d16a66
eduardooliveira <eos.eduardooliveira@gmail.com>
2024-02-20 16:57:50 -0300

    Initial setup

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { Plus } from 'lucide-react'
+
+export function App() {
+
+  return (
+    <div className="py-10 space-y-8">
+      <div>
+        header
+      </div>
+
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <button className='primary'>
+            <Plus className="size-3" />
+            Create new
+          </button>
+        </div>
+      </main>
+    </div>
+  )
+}
